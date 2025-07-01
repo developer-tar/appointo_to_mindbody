bfe7d4d22ccd5e543f0830b8dabca2e668d43556
Tarun kumar <tarunkumar@luminoguru.com>
2025-07-01 15:00:43 +0530

    Refactor: optimize GetAppointoAppointment command and remove unused code

--- app/Console/Commands/GetAppointoAppoinment.php
+++ app/Console/Commands/GetAppointoAppoinment.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
 use App\Jobs\UpdateAppointoAppointment;
 
 class GetAppointoAppoinment extends Command
